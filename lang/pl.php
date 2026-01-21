49b039ee0710338d2be00427ae2ca67d42aff477
ugovi00 <maciusmacieje21@gmail.com>
2026-01-21 22:09:47 +0100

    pl.php

---
+++ lang/pl.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	'login_title' => 'Logowanie',
+	'username' => 'Login',
+	'next' => 'Dalej',
+	'login' => 'Zaloguj',
+	'encrypted_msg' => 'Zaszyfrowana wiadomość',
+	'paste_decrypted' => 'Wklej odszyfrowaną treść',
+	'err_empty_login' => 'Wpisz login',
+	'err_empty_answer' => 'Wklej odszyfrowaną treść',
+	'err_connection' => 'Błąd połączenia z serwerem',
+	'err_pgp' => 'Błąd szyfrowania PGP',
+'ERR_NO_USERNAME'      => 'Brak loginu',
+'ERR_USER_NOT_FOUND'   => 'Użytkownik nie istnieje',
+'ERR_NO_PGP_KEY'       => 'Brak klucza PGP',
+'ERR_MISSING_DATA'     => 'Brak danych',
+'ERR_INVALID_RESPONSE' => 'Niepoprawna odpowiedź',
+'ERR_UNKNOWN_ACTION'   => 'Nieznana akcja',
+'home_title'  => 'Strona główna',
+'home_panel'  => 'Panel ',
+'welcome'     => 'Witaj',
+'logout'      => 'Wyloguj'
+];
